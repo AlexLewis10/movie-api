7d2cb03f049d383dd0b8a30ca787cbc051e3d758
Alex Lewis <alexlewis374@gmail.com>
2020-05-02 13:47:21 +0100

    api up and running

--- scripts.js
+++ scripts.js
@@ -8,6 +8,24 @@ request.open('GET', 'https://ghibliapi.herokuapp.com/films', true)
 
 request.onload = function() {
 // this is where you can access the JSON data 
+    var data = JSON.parse(this.response)
+    if (request.status >= 200 && request.status < 400) {
+        data.forEach(movie => {
+            console.log(movie.title)
+        }) 
+    } else {
+        console.log('error')
+    }
 }
 
+//we send the request
 request.send()
+
+const app = document.getElementById('root')
+console.log(app)
+
+
+
+
+
+
