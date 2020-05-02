63e3483bde2429f21d5aa331481db1acbba9bcb9
Alex Lewis <alexlewis374@gmail.com>
2020-05-02 14:38:43 +0100

    add extra divs from scripts.js

--- scripts.js
+++ scripts.js
@@ -11,18 +11,49 @@ request.onload = function() {
     var data = JSON.parse(this.response)
     if (request.status >= 200 && request.status < 400) {
         data.forEach(movie => {
-            console.log(movie.title)
+            const card = document.createElement('div')
+            card.setAttribute('class', 'card')
+
+            const h1 = document.createElement('h1')
+            h1.textContent = movie.title
+            
+            const p = document.createElement('p')
+            movie.description = movie.description.substring(0, 300)
+            p.textContent = `${movie.description}...`
+            container.appendChild(card)
+            card.appendChild(h1)
+            card.appendChild(p)
+
+
+            // console.log(movie.title)
+            // console.log(movie.description)
         }) 
     } else {
-        console.log('error')
+        // console.log('error')
+        const errorMessage = document.createElement('marquee')
+        errorMessage.textContent = "API error"
+        app.appendChild(errorMessage)
     }
 }
 
 //we send the request
 request.send()
 
+//getting the div root and setting in to a constant
 const app = document.getElementById('root')
-console.log(app)
+//We are going to have an image tag in the html. 
+//So we use the createElement method on document
+const logo = document.createElement('img')
+logo.src = 'images/logo.png'
+//again we are going to create another element div
+//we then set the class attribute to container
+const container = document.createElement('div')
+container.setAttribute('class', 'container')
+//now there is a logo and a container, we need to place them on the website
+//we use appendChild to append both logo and container div to the app root
+app.appendChild(logo)
+app.appendChild(container)
+
 
 
 
